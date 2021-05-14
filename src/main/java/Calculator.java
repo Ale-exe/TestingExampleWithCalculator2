162eb4a680d1ed30c425ae40c8a40a77def2233a
Alex Gwilliam <alexlgwilliam@gmail.com>
2021-05-14 12:24:38 +0100

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+
+
+
+public class Calculator
+{
+    public double multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public static void main(String[] args)
+    {
+        Calculator calcy = new Calculator();
+        System.out.println(calcy.multiply(0,7));
+
+    }
+}
